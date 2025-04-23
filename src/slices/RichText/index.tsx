32c5646649d6d4b54fa87d64a4f0c35961473a6d
Yuki Terasaka <yuki.terasaka@gmail.com>
2025-04-23 10:32:03 +0900

    initil commit

---
+++ src/slices/RichText/index.tsx
@@ -0,0 +1,38 @@
+import { type FC } from "react";
+import { type Content } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
+import {
+  PrismicRichText,
+  type SliceComponentProps,
+  type JSXMapSerializer,
+} from "@prismicio/react";
+import styles from "./index.module.css";
+
+const components: JSXMapSerializer = {
+  hyperlink: ({ node, children }) => {
+    return <PrismicNextLink field={node.data}>{children}</PrismicNextLink>;
+  },
+  label: ({ node, children }) => {
+    if (node.data.label === "codespan") {
+      return <code>{children}</code>;
+    }
+  },
+};
+
+/**
+ * Props for `RichText`.
+ */
+type RichTextProps = SliceComponentProps<Content.RichTextSlice>;
+
+/**
+ * Component for "RichText" Slices.
+ */
+const RichText: FC<RichTextProps> = ({ slice }) => {
+  return (
+    <section className={styles.richtext}>
+      <PrismicRichText field={slice.primary.content} components={components} />
+    </section>
+  );
+};
+
+export default RichText;
